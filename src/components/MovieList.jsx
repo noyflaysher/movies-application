77e8b89ed77b59ef6d501c6ad3d5406306b17b31
noyflaysher <noyflaysher12@gmail.com>
2022-11-27 15:49:41 +0200

    crete movie list

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MovieList(props) {
+  return (
+    <>
+    {props.movies.map((movie,index)=>(
+        <div className='image-container d-flex justify-content-start m3'>
+            <img src={movie.Poster} alt="movie" />
+        </div>
+    ))}</>
+  )
+}
+
+export default MovieList
